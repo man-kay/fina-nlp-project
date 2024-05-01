f50b0714c2205492a9d752b3122a3ea4d5381bba
Hero <herotai214@gmail.com>
2024-05-02 05:20:37 +0800

    Added vanilla_ESGBert notebook at /notebook

--- conda-env-with-cuda.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pip install ipykernel ipywidgets &&
-pip install pandas pyarrow &&
-conda install cudatoolkit=11.8 &&
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118 && 
-pip install transformers peft accelerate
-pip install trl xformers wandb datasets einops gradio sentencepiece bitsandbytes
