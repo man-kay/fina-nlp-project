78684b9c6c01d83342ebc7d61f176b30b2001037
man-kay <kathy09@connect.hku.hk>
2024-05-03 12:04:50 +0800

    renamed notebooks

--- deploy/deploy_model.py
+++ deploy/deploy_model.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
-df = pd.read_csv("../../data/predicted/combined_prediction.csv")
+df = pd.read_csv("../data/predicted/combined_prediction.csv")
 
 
 @app.route("/")
